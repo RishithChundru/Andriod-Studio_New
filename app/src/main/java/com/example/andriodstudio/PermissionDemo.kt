a21b9204827ae03cf36aee4d350339e48689e27c
Rishith <rishithrishith1@gmail.com>
2024-11-04 18:30:42 +0530

    new files

--- app/src/main/java/com/example/andriodstudio/PermissionDemo.kt
+++ app/src/main/java/com/example/andriodstudio/PermissionDemo.kt
@@ -1,8 +1,10 @@
 package com.example.andriodstudio
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -24,11 +26,25 @@ class PermissionDemo : AppCompatActivity() {
             if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)==PackageManager.PERMISSION_DENIED){
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),c)
             }
-            if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_WIFI_STATE)==PackageManager.PERMISSION_DENIED){
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_WIFI_STATE),c)
-            }
             else{
                 Toast.makeText(this,"camera permission already granted",Toast.LENGTH_SHORT).show()
+                val i2 = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivity(i2)
+            }
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==c){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Camera Permission granted",Toast.LENGTH_SHORT).show()
+                val i2 = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivity(i2)
+            }
+            else{
+                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
