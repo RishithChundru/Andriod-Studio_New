7d6612570bc3a4f8a266c759d37e4c2139256292
Rishith <rishithrishith1@gmail.com>
2024-11-13 22:03:43 +0530

    new files

--- app/src/main/java/com/example/andriodstudio/ContextMenu.kt
+++ app/src/main/java/com/example/andriodstudio/ContextMenu.kt
@@ -1,12 +1,15 @@
 package com.example.andriodstudio
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.ContextMenu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,5 +29,22 @@ class ContextMenu : AppCompatActivity() {
         super.onCreateContextMenu(menu, v, menuInfo)
         val i1: MenuInflater=menuInflater
         i1.inflate(R.menu.context_menu,menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        var i=findViewById<ImageView>(R.id.imageView5)
+        return when (item.itemId ) {
+            R.id.change_color -> {
+                i.setColorFilter(Color.RED)
+                Toast.makeText(this, "Image Colour Changed to Red",Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.rotate -> {
+                i.rotation=i.rotation+45f
+                Toast.makeText(this,"Image was Rotated by 45 degrees",Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
     }
 }
