30a1bc4bdbbb1e0492c59efbabbf58802784b61b
Rishith <rishithrishith1@gmail.com>
2024-11-26 22:36:35 +0530

    new files

--- app/src/main/java/com/example/andriodstudio/optionMenu.kt
+++ app/src/main/java/com/example/andriodstudio/optionMenu.kt
@@ -1,20 +1,39 @@
 package com.example.andriodstudio
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class optionMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_option_menu2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val t=findViewById<Toolbar>(R.id.toolbar2)
+        setSupportActionBar(t)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.optionmenu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem):Boolean{
+        return when(item.itemId){
+            R.id.s3->{
+                Toast.makeText(this,"settings clicked",Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.s4->{
+                Toast.makeText(this,"about us clicked",Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
