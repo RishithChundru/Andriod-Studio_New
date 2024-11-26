a33813241eed3bba7b4fe526c6e7b91b37ab9b67
Rishith <rishithrishith1@gmail.com>
2024-11-26 09:36:52 +0530

    new files

--- app/src/main/java/com/example/andriodstudio/Intent_login_1.kt
+++ app/src/main/java/com/example/andriodstudio/Intent_login_1.kt
@@ -1,20 +1,37 @@
 package com.example.andriodstudio
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Intent_login_1 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_intent_login1)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var a=findViewById<EditText>(R.id.editTextText9)
+        var b=findViewById<EditText>(R.id.editTextText10)
+        var c=findViewById<Button>(R.id.button42)
+        c.setOnClickListener{
+            var uname=a.text.toString()
+            var pass=b.text.toString()
+            if(uname=="Rishith" && pass=="Rishith2005"){
+                val intent= Intent(this,Intent_login_2::class.java)
+                intent.putExtra("Extra message",uname)
+                startActivity(intent)
+                Toast.makeText(this,"Login successfully",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"Login credentials are wrong",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/andriodstudio/Intent_login_2.kt
+++ app/src/main/java/com/example/andriodstudio/Intent_login_2.kt
@@ -1,20 +1,24 @@
 package com.example.andriodstudio
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Intent_login_2 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_intent_login2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        var a=findViewById<TextView>(R.id.textView21)
+        var msg=intent.getStringExtra("Extra message")
+        a.text="Welcome to page $msg"
+        Toast.makeText(this,"use the app",Toast.LENGTH_SHORT).show()
     }
 }
